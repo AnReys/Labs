e56fa90bd3e92356479340d74d544c1e810668db
AnReys <Reis.a.s@nmu.one>
2021-11-21 10:22:06 +0200

    v_3

--- src/main/java/lab2/controllers/HumanCreator.java
+++ src/main/java/lab2/controllers/HumanCreator.java
@@ -8,16 +8,16 @@ import java.util.List;
 import java.util.Random;
 
 public class HumanCreator {
-    private static final List<String> dictionaryNames = Arrays.asList("Ann", "Kostya", "Vitaliy", "Dima", "Eva", "Natan", "Pizduck");
-    private List<String> dictionarySurnames = Arrays.asList("Faice", "Nikon", "Duck", "Fortune", "Kindza", "Ravi", "Shora");
-    private List<String> dictionaryDateOfBirth = Arrays.asList("13/15/1995", "09/08/1992", "02/03/2000", "24/12/1998", "13/06/1955", "31/04/2001", "28/07/1997");
+    public static final List<String> DICTIONARY_NAMES = Arrays.asList("Ann", "Kostya", "Vitaliy", "Dima", "Eva", "Natan", "Pizduck");
+    public static final List<String> DICTIONARY_SURNAMES = Arrays.asList("Faice", "Nikon", "Duck", "Fortune", "Kindza", "Ravi", "Shora");
+    public static final List<String> DICTIONARY_DATE_OF_BIRTH = Arrays.asList("13/15/1995", "09/08/1992", "02/03/2000", "24/12/1998", "13/06/1955", "31/04/2001", "28/07/1997");
 
     public Human createHumanRandomly(){
         Random random = new Random();
         Human human = new Human();
-        human.setName((dictionaryNames.get(random.nextInt(dictionaryNames.size()-1))));
-        human.setSurname((dictionarySurnames.get(random.nextInt(dictionaryNames.size()-1))));
-        human.setDateOfBirth((dictionaryDateOfBirth.get(random.nextInt(dictionaryNames.size()-1))));
+        human.setName((DICTIONARY_NAMES.get(random.nextInt(DICTIONARY_NAMES.size()-1))));
+        human.setSurname((DICTIONARY_SURNAMES.get(random.nextInt(DICTIONARY_SURNAMES.size()-1))));
+        human.setDateOfBirth((DICTIONARY_DATE_OF_BIRTH.get(random.nextInt(DICTIONARY_DATE_OF_BIRTH.size()-1))));
 
         return human;
 
